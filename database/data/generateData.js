e0099c554810ed9599358a5f15e700dd438d6e6f
Jonathan <jonathanholson@gmail.com>
2019-11-01 17:22:10 -0700

    Created Data-Generation File; Writes to 3 CSVs: Songs, Likes, Play History

--- database/data/generateData.js
+++ database/data/generateData.js
@@ -9,41 +9,127 @@ const likesCSV =
 const playHistoryCSV =
   '/Users/jonathanolson/HackReactor/SDC/playbar/database/data/playHistory.csv';
 
-// WRITE HEADERS
-const songsHeaders = [
-  'songId',
-  'likeCount',
-  'songDataURL',
-  'songName',
-  'artist',
-  'album',
-  'thumbnailURL',
-  '\n\r',
-];
-fs.writeFileSync(songsCSV, songsHeaders, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('song headers written successfully!!');
-  }
-});
+// Generate list of 1000 users
+const users = [];
 
-// GENERATE AND SAVE SONG EXAMPLES
-const genSaveSongs = () => {
-  for (let i = 0; i < 1000; i += 1) {
-    // Create song
+for (let i = 0; i < 1000; i += 1) {
+  users.push(faker.internet.userName());
+}
+
+const writeHeaders = () => {
+  // WRITE HEADERS
+  const songsHeaders = [
+    'songId',
+    'likeCount',
+    'songDataURL',
+    'songName',
+    'artist',
+    'album',
+    'thumbnailURL',
+    '\n\r',
+  ];
+  const likeHeaders = ['songId', 'userName', '\n\r'];
+  const playHistoryHeaders = ['songId', 'userName', '\n\r'];
+
+  fs.writeFileSync(songsCSV, songsHeaders, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('song headers written successfully!!');
+    }
+  });
+
+  fs.writeFileSync(likesCSV, likeHeaders, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('like headers written successfully!!');
+    }
+  });
+
+  fs.writeFileSync(playHistoryCSV, playHistoryHeaders, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('play history headers written successfully!!');
+    }
+  });
+};
+
+const generateAndSaveData = () => {
+  // GENERATE AND SAVE SONG EXAMPLES
+  for (let i = 0; i < 100; i += 1) {
+    // get a username
+    const userName =
+      users[
+        faker.random.number({
+          min: 0,
+          max: 999,
+        })
+      ];
+    // Create song; songId = i
     const song = [
-      faker.internet.userName(),
-      faker.random.number(),
-      faker.image.imageUrl(),
-      faker.random.words(),
-      faker.internet.userName(),
-      faker.random.word(),
-      faker.image.imageUrl(),
+      i, // song id
+      faker.random.number(), // like count
+      faker.image.imageUrl(), // song data URL
+      faker.random.words(), // song name
+      faker.internet.userName(), // artist
+      faker.random.word(), // album
+      faker.image.imageUrl(), // thumbnail url
       '\r',
     ];
-    // write song object to file
+    // write song array to file
     fs.appendFileSync(songsCSV, song, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  // GENERATE AND SAVE LIKE EXAMPLES
+  for (let i = 0; i < 100; i += 1) {
+    // get a song id < 10,000,000
+    const songId = faker.random.number({
+      min: 0,
+      max: 9999999,
+    });
+    // get a username
+    const userName =
+      users[
+        faker.random.number({
+          min: 0,
+          max: 999,
+        })
+      ];
+    // Create like entry
+    const likeEntry = [songId, userName, '\r'];
+    // write like array to file
+    fs.appendFileSync(likesCSV, likeEntry, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  // GENERATE AND SAVE PLAY HISTORY EXAMPLES
+  for (let i = 0; i < 100; i += 1) {
+    // get a song id
+    const songId = faker.random.number({
+      min: 0,
+      max: 9999999,
+    });
+    // get a username
+    const userName =
+      users[
+        faker.random.number({
+          min: 0,
+          max: 999,
+        })
+      ];
+    // Create history entry
+    const historyEntry = [songId, userName, '\r'];
+    // write history array to file
+    fs.appendFileSync(playHistoryCSV, historyEntry, (err) => {
       if (err) {
         console.log(err);
       }
@@ -51,12 +137,5 @@ const genSaveSongs = () => {
   }
 };
 
-genSaveSongs();
-
-// GENERATE AND SAVE LIKES DATA
-
-const genSaveLikes = () => {};
-
-// GENERATE AND SAVE PLAY HISTORY DATA
-
-const genSavePlayHIstory = () => {};
+writeHeaders();
+generateAndSaveData();
